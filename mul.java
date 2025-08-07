aecae8e69b76022e0b279178250872690e0d9d91
Pranjal Pandey <pranjal3210@gmail.com>
2025-08-07 14:45:46 +0530

    Initial commit with README

---
+++ mul.java
@@ -0,0 +1,11 @@
+package calculator;
+
+// Example of a simple multiplication
+public class mul {
+    public static void main(String[] args) {
+        double number1 = 10.5;
+        double number2 = 4.2;
+        double result = number1 * number2;
+        System.out.println("The product is: " + result); // Output: The product is: 44.1
+    }
+}
